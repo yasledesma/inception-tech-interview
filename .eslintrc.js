df5a81cba4ca82e443d552db54fb09288dba57fe
Yasmin Ledesma <yaslesd@gmail.com>
2024-02-08 22:48:44 -0300

    fix(config): fix ecmaVersion type

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,10 @@
 module.exports = {
   "env": {
       "browser": true,
-      "es2021": true
   },
   "extends": "eslint:recommended",
   "parserOptions": {
-      "ecmaVersion": "latest",
+      "ecmaVersion": 2021,
       "sourceType": "module"
   },
 }
