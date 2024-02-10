c9ca84e9430a337401d456931abfbdd6a5192895
Yasmin Ledesma <yaslesd@gmail.com>
2024-02-09 21:21:55 -0300

    feat(navbar): add Navbar component
    Add 'Navbar' component to main layout.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
+    <navbar />   
     <v-main>
       <router-view />
     </v-main>
@@ -31,8 +8,13 @@
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
 export default {
   name: "App",
+
+  components: {
+    Navbar,
+  },
 
   data: () => ({
     //
---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+    data() {
+        const opts = [
+            {name: "Home", path: "/"},
+            {name: "About", path: "/about"},
+            {name: "Data", path: "/data"},
+        ];
+        
+        const model = opts.map(opt => opt.path).indexOf(window.location.pathname);
+        
+        return {
+            opts,
+            model
+        }
+    },
+    methods: {
+        go(path) {
+            this.$router.push(path);
+        },
+    }
+}
+</script>
+
+<template>
+  <v-app-bar
+    app
+    color="green"
+    hide-on-scroll
+  >
+    <v-list 
+      color="green"
+      dense
+      class="d-flex flex-row"
+    >
+      <v-icon
+        x-large
+        class="mx-6"
+      >
+        mdi-cat
+      </v-icon>
+      
+      <v-list-item-group
+        v-model="model"
+        group="main"
+        mandatory
+        class="d-flex flex-row"
+      >
+        <v-list-item
+          v-for="opt in opts"
+          :key="opt.name"
+          v-on:click="go(opt.path)"
+          v-on:keyup.enter="go(opt.path)"
+        >
+          <v-list-item-title>
+            {{ opt.name }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-app-bar>
+</template>
