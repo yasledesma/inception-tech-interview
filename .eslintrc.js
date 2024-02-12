dbafc3e77f615deca60528d209309eb3f9fe7ec9
Yasmin Ledesma <yaslesd@gmail.com>
2024-02-12 20:06:16 -0300

    test(tests): add test environment config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,34 @@ module.exports = {
         "browser": "true",
         "node": "true"
     },
+
     "extends": [
         "eslint:recommended",
         "plugin:vue/recommended",
         "prettier"
     ],
+
     "rules": {
         "vue/no-unused-vars": "error",
         "vue/v-on-style": "off"
     },
+
     "parser": "vue-eslint-parser",
+
     "parserOptions": {
         "ecmaVersion": 8,
         "sourceType": "module"
     },
+
+    overrides: [
+      {
+        files: [
+          '**/__tests__/*.{j,t}s?(x)',
+          '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        ],
+        env: {
+          jest: true
+        }
+      }
+    ]
 }
