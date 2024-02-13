481314f2b4f9e2f4b467012ccf3d472c2238bef0
Yasmin Ledesma <yaslesd@gmail.com>
2024-02-12 21:17:32 -0300

    feat(about): add AboutMe section for About page

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container fuild>
+    <v-col>
+      <v-col>
+        <h2>Hi there!</h2>
+      </v-col>
+      <v-col>
+        <v-body-1>
+          Glad to see you're looking at this page. I figured since the project came with an "about" page, I would not let it go blank and use it to say some stuff about me here.
+          <br>
+          <strong>My name is Yasmin, and I'm a software developer from Argentina.</strong> I currently do fullstack web development, but I specialize in backend stuff, mostly. So, sorry if this project ends up being more function over form; so to speak. I do a better job using Figma files.
+          <br>
+          Also, I do have a few cats of my own, but posting pictures of them this early into an interview process is not my thing, to be honest. So here are a few random gif I have in my images folder!
+        </v-body-1>
+      </v-col>
+    </v-col>
+    <v-col>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-img
+            alt="Cat with face of surprise."
+            contain
+            src="../assets/cat-1.gif"
+          />
+        </v-col>
+        <v-col
+          cols="12"
+          md="3"
+        >
+          <v-img
+            alt="Bart Simpson performing an April's fool prak. Gone wrong."
+            contain
+            src="../assets/bart.gif"
+          />
+        </v-col>
+        <v-col
+          cols="12"
+          md="2"
+        >
+          <v-img
+            alt="Cat getting hit by another cat."
+            contain
+            src="../assets/cat-2.gif"
+          />
+        </v-col>
+        <v-col
+          cols="12"
+          md="2"
+        >
+          <v-img
+            alt="Mike Wazowski making some kind of expression."
+            contain
+            src="../assets/wazowski.jpeg"
+          />
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-container>
+</template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <AboutMe />
 </template>
+
+<script>
+  import AboutMe from "../components/AboutMe.vue";
+    
+  export default {
+    name: "About",
+      
+    components: {
+      AboutMe,
+    },
+  };
+</script>
